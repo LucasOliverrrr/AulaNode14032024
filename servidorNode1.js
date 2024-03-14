92d96c628dbf0c2fa8522d401ee3caaa4a435e58
LucasOliverrrr <lucasouze@gmail.com>
2024-03-14 09:50:23 -0300

    Servidor 1 feito

--- servidorNode1.js
+++ servidorNode1.js
@@ -1,1 +1,8 @@
-const http
+const http = require('http')
+
+http.createServer((requesicao, resposta)=>{
+      resposta.writeHead(200, {'Content-Type': 'text/plain'})
+      resposta.write('Ola mundooooooooooooo \n')
+      resposta.end()
+
+}).listen(3000)
